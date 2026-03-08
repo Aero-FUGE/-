37f75c9fc85e222aa1cd9defda8f418d9457b5a8
Aero-FUGE <2563247782@qq.com>
2026-03-08 22:28:58 +0800

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0B_ZOJ8uaW6iuLBKmiqEmJNxuOSBR-ro",
+  authDomain: "my-system-489607.firebaseapp.com",
+  databaseURL: "https://my-system-489607-default-rtdb.firebaseio.com",
+  projectId: "my-system-489607",
+  storageBucket: "my-system-489607.firebasestorage.app",
+  messagingSenderId: "544320925340",
+  appId: "1:544320925340:web:ad84df94be15bbb79a0759",
+  measurementId: "G-GZN73T3FJC"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
