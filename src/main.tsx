b656179261739f7370751e70d32e0adff95a3cdf
Aero-FUGE <2563247782@qq.com>
2026-03-08 22:16:16 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,24 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0B_ZOJ8uaW6iuLBKmiqEmJNxuOSBR-ro",
+  authDomain: "my-system-489607.firebaseapp.com",
+  databaseURL: "https://my-system-489607-default-rtdb.firebaseio.com",
+  projectId: "my-system-489607",
+  storageBucket: "my-system-489607.firebasestorage.app",
+  messagingSenderId: "544320925340",
+  appId: "1:544320925340:web:ad84df94be15bbb79a0759",
+  measurementId: "G-GZN73T3FJC"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
